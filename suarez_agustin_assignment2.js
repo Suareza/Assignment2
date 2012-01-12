8bd1b707805ce7cca5d8aa2fa9074e8fbd13c85c
Agustin Suarez <suareza@fullsail.edu>
2012-01-12 11:49:21 -0800

    Boolean Function.

--- suarez_agustin_assignment2.js
+++ suarez_agustin_assignment2.js
@@ -7,18 +7,36 @@
 
 var toQualify = "Matchup",
     numberOfMatches = 6,
-    myChoiceTeams = ["Real Madrid", "Manchester United", "FC Barcelona"];
+    myChoiceTeams = ["Real Madrid", "Manchester United", "FC Barcelona"],
+    OptionB_Money = 75;
 
 // Procedure
+console.log("I began to notice with more detail the options for the Tournament:");
 var selection = function(First){
-        console.log("There are three options to enter the FIFA 12 tournament but I choose " + First + ". This option consists of winning 4 games back to back to qualify to finals.");
-        console.log("This seems pretty easy right?");
+        console.log("There are three options to enter the FIFA 12 tournament. " + First + " consists of winning 4 games back to back to qualify to finals.");
         var passOptionA = true;
             if (passOptionA === true){
-                console.log("I was so exicted to see the: Congratualtions you have earned a spot for the finals!!!")
+                console.log("Display: Congratualtions you have earned a spot for the finals!!!")
             } else {
-                console.log("Game Over... I have to start all over again.")
+                console.log("Display: Game Over... start all over again.")
             };
 };
-
 selection("Option A");
+
+// Boolean Function
+var optionB = function (optionB_Money, matches){
+      console.log("The second option was Option B. This option cost $" + optionB_Money + " to enter plus you had to win " + matches + " matches back to back to qualify to finals.");
+      
+      var winThreeGames = true,
+          entryFee = OptionB_Money,
+          loseOneGame = false,
+          passOptionA = true
+      
+      if (entryFee && winThreeGames || passOptionA) {
+            console.log("Congratulations you qualify for Finals!!");
+        } else {
+            console.log("Game Over... please choose different options if you wish to continue with Tournament.");
+        }
+
+};
+optionB(75, 3);
