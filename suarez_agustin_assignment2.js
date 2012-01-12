8c1ccf99dc1b752522891069cba2c4cf3d275a6b
Agustin Suarez <suareza@fullsail.edu>
2012-01-12 10:37:55 -0800

    Added file name and variables.

--- suarez_agustin_assignment2.js
+++ suarez_agustin_assignment2.js
@@ -1,1 +1,10 @@
-alert("JavaScript works!");
+/*
+ Agustin Suarez
+ January 12, 2011
+ Assignment 2
+ "Competition"
+*/
+
+var toQualify = "Matchup",
+    numberOfMatches = 6,
+    myChoiceTeams = ["Real Madrid", "Manchester United", "FC Barcelona"];
